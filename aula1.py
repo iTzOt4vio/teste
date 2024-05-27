2536560981daf6d7f88237f92774a50ea5773f40
Otavio Heguedusch <00001091671746sp@aluno.educacao.sp.gov.br>
2024-05-27 09:13:04 -0300

    Class livraria

--- aula1.py
+++ aula1.py
@@ -1,4 +1,30 @@
-teste = 1
+class Livraria:
+    def __init__(self):
+        self.estoque = {}
 
-local = 59
-print("oiii")
+    def adicionar_livro(self, titulo, quantidade):
+        if titulo in self.estoque:
+            self.estoque[titulo] += quantidade
+        else:
+            self.estoque[titulo] = quantidade
+
+    def remover_livro(self, titulo, quantidade):
+        if titulo in self.estoque:
+            if self.estoque[titulo] >= quantidade:
+                self.estoque[titulo] -= quantidade
+                if self.estoque[titulo] == 0:
+                    del self.estoque[titulo]
+                return True
+            else:
+                print(f"Não há estoque suficiente para remover {quantidade} cópias de '{titulo}'.")
+        else:
+            print(f"O livro '{titulo}' não está no estoque.")
+        return False
+
+    def listar_livros(self):
+        if self.estoque:
+            print("Livros disponíveis:")
+            for titulo, quantidade in self.estoque.items():
+                print(f"{titulo}: {quantidade} cópias")
+        else:
+            print("Bosta")
